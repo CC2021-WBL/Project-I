ab30363b84823c558a86853a9d2c74b8bbca2520
Monika Krella <monika.krella@gmail.com>
2021-12-01 18:26:42 +0100

    Changes: -added validation for variables, which will be getting from outside -added export possibility -fixed bug about ID numerations (in array from data indexes =  are from 0)

--- src/app/questionGenerator.js
+++ src/app/questionGenerator.js
@@ -1,12 +1,13 @@
-class QuestionGenerator {
+export class QuestionGenerator {
 
     //argument argAmountOfId will be relaced by connection to CurrentMode and it's IDs'
     static generateQuestionsForCurrentMode(amountOfId) {
         const amountOfAnswersPerQuestion = 4;
         let generatedIds = [];
 
-        if (amountOfId < amountOfAnswersPerQuestion) {
-            throw `amountOfId must be greater or equal ${amountOfAnswersPerQuestion}. Passed value: ${amountOfId}`
+        if (amountOfId < amountOfAnswersPerQuestion || typeof(amountOfId) !== `number` ) {
+            throw `Check amout of ID's and datatype. amountOfId must be greater or equal ${amountOfAnswersPerQuestion}. Passed value: ${amountOfId}
+            Type must be number. Passed type: ${typeof(amountOfId)}`
         }
 
         for (let index = 0; index < amountOfAnswersPerQuestion; index++) {
@@ -23,11 +24,26 @@ class QuestionGenerator {
     }
 
     static chooseCorrectAnswer(arrayWithId = []) {
-        const minIndex = 0;
-        const maxIndex = 3;
-        const index = Math.floor(Math.random() * (maxIndex - minIndex + 1) + minIndex);
-        const rightAnswer = arrayWithId[index];
-        return rightAnswer;
+        let IsNumberAsValuetype = true;
+        arrayWithId.forEach(element => {
+            if (typeof(element) !== 'number') {
+                IsNumberAsValuetype = false;
+                throw `wrong datatype in array`
+        
+            }
+        });
+       
+        if (arrayWithId.length !== 4) {
+          throw `4 elements needed. That array has ${arrayWithId.length} elements`
+        }
+        else {
+          const minIndex = 0;
+          const maxIndex = 3;
+          const index = Math.floor(Math.random() * (maxIndex - minIndex + 1) + minIndex);
+          const rightAnswer = arrayWithId[index];
+          return rightAnswer; 
+        }
+        
         
     }
    
@@ -36,9 +52,9 @@ class QuestionGenerator {
 
 // ------------------PRIVATE FUNCTIONS----------------------------------------
 
-
+//internal function just for another functions in that file. No validation here -> validation is in generateQuestionsForCurrentMode funcion
 function generateRandomId(amountOfId) {
-    const minId = 1;
-    return Math.floor(Math.random() * (amountOfId - minId + 1) + minId);
+      const minId = 0;
+      return Math.floor(Math.random() * (amountOfId - minId + 1) + minId); 
+    
 }
-
