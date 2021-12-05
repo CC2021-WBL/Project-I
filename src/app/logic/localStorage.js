d25c6219fced5e19c9b41d35bd3d6a1e3dc84c9c
Smyku6 <smyku6@gmail.com>
2021-12-05 12:48:34 +0100

    working on localstorage feature

--- src/app/logic/localStorage.js
+++ src/app/logic/localStorage.js
@@ -1,8 +1,31 @@
-const testObject = {
+const testObject1 = {
   name: 'Albus Dumbledore',
-  result: 200,
+  result: 225,
   mode: 'Characters',
 };
+const testObject2 = {
+  name: 'Severus Snape',
+  result: 125,
+  mode: 'Characters',
+};
+
+const testObject3 = {
+  name: 'Rubeus Hagrid',
+  result: 300,
+  mode: 'Characters',
+};
+
+const rankings = [];
+
+function addResult(resultObject) {
+  rankings.push(resultObject);
+}
+
+addResult(testObject1);
+addResult(testObject2);
+addResult(testObject3);
+
+console.log(rankings);
 
 function _saveGameInfoInLocalStorage(object) {
   localStorage.setItem('gameinfo', JSON.stringify(object));
