bbffa18fa28d18b17511baa98c7a77dc0c76376f
Alicja Kondrat <pierwszazlewej@gmail.com>
2021-12-07 18:10:36 +0100

    red and white buttons

--- src/app/view/redButton..js
+++ src/app/view/redButton..js
@@ -1,6 +1,6 @@
 export default function ButtonRed(text) {
   const newButtonRed = document.createElement('button');
-  newButtonRed.classList.add('button', //'RedButtonClassName'//);
+  newButtonRed.classList.add('button', game__play);
   newButtonRed.innerText = text;
   return newButtonRed;
 }
---
+++ src/app/view/whiteButton.js
@@ -0,0 +1,7 @@
+export default function ButtonWhite(text) {
+  const ButtonWhite = document.createElement('button');
+  newButtonWhite.classList.add('game__btn');
+  newButtonWhite.innerText = text;
+  return newButtonWhite;
+}
+
