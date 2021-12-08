6960d48d25caae8662052d0738c42ae657a2d701
Klaudia <kosaa96@gmail.com>
2021-12-08 10:23:39 +0100

    Test function

--- src/app/logic/correctAnswer.js
+++ src/app/logic/correctAnswer.js
@@ -1,3 +1,7 @@
 const isAnswerCorrect = (correctAnswer, chosenAnswer) => {
   return correctAnswer === chosenAnswer;
 };
+
+//Test//
+// console.log(isAnswerCorrect("qwe", "qwe")); // true
+// console.log(isAnswerCorrect("Qwe", "qwe")); //false
