9461ca16f12ac1c0cdb7d28f8f8cb6f5f4b25dde
Monika Krella <monika.krella@gmail.com>
2021-12-08 22:30:13 +0100

    repair some bugs in code. changed downloaded image to downloaded url link of photo

--- src/app/logic/dataManager.js
+++ src/app/logic/dataManager.js
@@ -1,5 +1,3 @@
-
-
 export class DataManager {
   constructor(IDsArray = [], resourceAPIadress, idOfRightAnswer) {
     this.idOfRightAnswer = idOfRightAnswer;
@@ -8,7 +6,7 @@ export class DataManager {
     this.reader = new FileReader();
     this.arraywithHPObjects;
     this.rightAnswer;
-    this.base64dataImgForRightAnswer;
+    this.urlOfImgRightAnswer;
     this.arraywithAnswersForQuestion = [];
   }
 
@@ -20,14 +18,17 @@ export class DataManager {
         this.arraywithHPObjects !== undefined
       ) {
         this.arraywithAnswersForQuestion = getDataForIDs(IDsArray);
+        
 
         // data for right answer:
         let rightAnswerValues = await getDataForCorrectAnswer(
           this.idOfRightAnswer,
           this.arraywithHPObjects,
         ).json();
+        
         this.rightAnswer = rightAnswerValues[0];
-        this.base64dataImgForRightAnswer = rightAnswerValues[1];
+        this.urlOfImgRightAnswer = rightAnswerValues[1];
+        
       }
       // Getting data first time and creating array with answers, img and name for correct answer
       else {
@@ -38,6 +39,7 @@ export class DataManager {
           this.IDsArray,
           this.arraywithHPObjects,
         );
+        
 
         //img for right answer:
         let rightAnswerValues = await getDataForCorrectAnswer(
@@ -45,7 +47,7 @@ export class DataManager {
           this.arraywithHPObjects,
         );
         this.rightAnswer = rightAnswerValues[0];
-        this.base64dataImgForRightAnswer = rightAnswerValues[1];
+        this.urlOfImgRightAnswer = rightAnswerValues[1];
       }
     } catch (error) {
       console.log(error);
@@ -53,18 +55,6 @@ export class DataManager {
   }
 }
 
-// ------------------------------------------- PART FOR JUST FOR TESTING, COMMENTED NOW-------------------------------------------
-
-//const temporaryResourceAPIadressToTest =
-//'http://hp-api.herokuapp.com/api/characters';
-
-//const dataManager = new DataManager(
-//[0, 1, 2, 3],
-//temporaryResourceAPIadressToTest,
-//0,
-//);
-
-//let something = await dataManager.getDataByAPI();
 
 //----------------------------------------------------------------PRIVATE METHODS-----------------------------------------------------
 //1) CREATING ARRAY WITH ANSWERS = NAMES OF HARRY POTTER CHARACTERS
@@ -80,33 +70,15 @@ function getDataForIDs(IDsArray = [], arraywithHPObjects) {
   return arrayWithAnswers;
 }
 
-//---------- IN PROGRESS-----------------------------------
-//2) GETTING NAME OF CHARACTER AND IMAGE FOR CORRECT ANSWER
+//2) GETTING NAME OF CHARACTER AND URL FOR IMAGE FOR CORRECT ANSWER
 
 async function getDataForCorrectAnswer(
   idOfRightAnswer,
   arraywithHPObjects = [],
 ) {
-  let rightAnswer;
-  let base64dataImg;
-
   const obj = arraywithHPObjects[idOfRightAnswer];
-  const name = obj.name;
-  rightAnswer = name;
+  const rightAnswer = obj.name;
   const imgUrl = obj.image;
-  //--------------------ACCESS TO FETCH BLOCKED BY CORS POLICY ---------------------------------------
-  //const imgFromWeb = await fetch(
-  //"https://cors-anywhere.herokuapp.com/http://hp-api.herokuapp.com/images/luna.jpg"
-  // );
-  //console.log(imgFromWeb);
-  //const imgBlob = await imgFromWeb.blob();
-  //console.log(imgBlob);
-  // reader.readAsDataURL(imgBlob);
-  //reader.onloadend = function () {
-  //base64dataImg = reader.result;
-  //console.log(base64dataImg);
-  //};
-  base64dataImg = 'base64shouldbehere';
 
-  return [rightAnswer, base64dataImg];
+  return [rightAnswer, imgUrl];
 }
--- src/app/logic/mainQuestionManager.js
+++ src/app/logic/mainQuestionManager.js
@@ -2,39 +2,52 @@ import { QuestionGenerator } from './questionGenerator.js';
 import { DataManager } from './dataManager.js';
 
 //--------variables for testing, to be replaced in future---------------------------------------------------
-let urlAdress = 'http://hp-api.herokuapp.com/api/characters';
-let amountOfIDs = 50;
+let urlAdress1 = 'http://hp-api.herokuapp.com/api/characters';
+let urlAdress2 = 'http://hp-api.herokuapp.com/api/characters/students';
+let urlAdress3 = 'http://hp-api.herokuapp.com/api/characters/staff';
+let amountOfIDs = 30;
 //---------------------------------------------------------------------------------------------------------------
 
 export class MainQuestionManager {
   constructor(urlAdress, amountOfIDs) {
     this.urlAdress = urlAdress;
     this.amountOfIDs = amountOfIDs;
-    
+    this.arrayWithObjectsForMode = [];
   }
 
-  GetQuestion() {
-    let arrayWithID =
-      QuestionGenerator.generateQuestionsForCurrentMode(this.amountOfIDs);
+  async GetQuestion() {
+    let arrayWithID = QuestionGenerator.generateQuestionsForCurrentMode(
+      this.amountOfIDs,
+    );
     let idOfCorrectAnswer = QuestionGenerator.chooseCorrectAnswer(arrayWithID);
 
     let dataManager = new DataManager(
       arrayWithID,
-      urlAdress,
+      this.urlAdress,
       idOfCorrectAnswer,
     );
     await dataManager.getDataByAPI();
     let arraywithAnswersForQuestion = dataManager.arraywithAnswersForQuestion;
     let rightAnswer = dataManager.rightAnswer;
-    let base64imgForRightAnswer = dataManager.base64dataImgForRightAnswer;
+    let imgUrlForRightAnswer = dataManager.urlOfImgRightAnswer;
+    this.arrayWithObjectsForMode = dataManager.arraywithHPObjects;
 
-    return question = {
-      image: base64imgForRightAnswer,
+    const question = {
+      image: imgUrlForRightAnswer,
       answers: arraywithAnswersForQuestion,
       rightAnswer: rightAnswer,
+    };
+
+    return question;
+  }
+
+  GetObjectsForMode() {
+    if (this.arrayWithObjectsForMode.length > 0) {
+      return this.arrayWithObjectsForMode;
+    } else {
+      console.log(
+        'arrayWithObjectsForMode is empty, lets start the game for the first time',
+      );
     }
-
-  };
- 
+  }
 }
-
