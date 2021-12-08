de3839b2a6dc5c67d71acf93a2361c428d72081b
Klaudia <kosaa96@gmail.com>
2021-12-08 23:04:17 +0100

    create logo  dynamically

---
+++ src/app/view/createLogo.js
@@ -0,0 +1,11 @@
+export const createLogo = () => {
+  const img = document.createElement('img');
+  document.querySelector('.header').appendChild(img);
+  console.log(img);
+  img.classList.add('.header__game-logo');
+  img.src = 'static/assets/img/logo.png';
+
+  img.addEventListener('click', () => {
+    //the page will be reload
+  });
+};
--- src/app/view/reloadPage.js
+++
@@ -1,7 +0,0 @@
-export const reloadPage = () => {
-  const img = document.querySelector('.header__game-logo');
-
-  img.addEventListener('click', () => {
-    //the page will be reload
-  });
-};
