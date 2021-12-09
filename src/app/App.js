0800106e2618a3faacba3e0d47c3c0bc88f6b22c
WeronikaBL <weronika@datenexperten.com>
2021-12-09 22:02:39 +0100

    fix: add logo

--- src/app/App.js
+++ src/app/App.js
@@ -1,6 +1,9 @@
+import createLogo from './view/createLogo';
+
 const App = ({ title, options }) => {
-  const quizTitle = document.createElement('h1');
-  quizTitle.textContent = document.querySelector('#hpquiz-app').append(title);
+  // const quizTitle = document.createElement('h1');
+  // quizTitle.textContent = document.querySelector('#hpquiz-app').append(title);
+  createLogo();
 };
 
 export default App;
--- src/app/view/createLogo.js
+++ src/app/view/createLogo.js
@@ -1,14 +1,19 @@
+import logo from '/static/assets/img/logo.png'
+
 export const createLogo = () => {
   const img = document.createElement('img');
-  document.querySelector('.header').appendChild(img);
   console.log(img);
-  img.classList.add('.header__game-logo');
-  img.src = 'static/assets/img/logo.png';
-
+  img.classList.add('.header__game-logo__img');
+  console.log(img);
+  img.src = logo;
+  console.log(img);
   img.addEventListener('click', () => {
-    //the page will be reload
+    // the page will be reload
   });
+  const header = document.querySelector('.header__game-logo');
+  header.append(img);
 };
 
-//Test
-//Call a function in App.js
+// Test
+// Call a function in App.js
+export default createLogo;
