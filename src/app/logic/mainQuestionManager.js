9eaaca0e42db66fb6df523a98eba1a4a903393d5
Monika Krella <monika.krella@gmail.com>
2021-12-09 19:58:43 +0100

    -remove unnecessary code for testing

--- src/app/logic/mainQuestionManager.js
+++ src/app/logic/mainQuestionManager.js
@@ -1,13 +1,6 @@
 import { QuestionGenerator } from './questionGenerator.js';
 import { DataManager } from './dataManager.js';
 
-//--------variables for testing, to be replaced in future---------------------------------------------------
-let urlAdress1 = 'http://hp-api.herokuapp.com/api/characters';
-let urlAdress2 = 'http://hp-api.herokuapp.com/api/characters/students';
-let urlAdress3 = 'http://hp-api.herokuapp.com/api/characters/staff';
-let amountOfIDs = 30;
-//---------------------------------------------------------------------------------------------------------------
-
 export class MainQuestionManager {
   constructor(urlAdress, amountOfIDs) {
     this.urlAdress = urlAdress;
