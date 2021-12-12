352325e8d35f73553d245d7251be4b3ef88c42c1
Monika Krella <monika.krella@gmail.com>
2021-12-12 13:45:08 +0100

    -added folder data with files config.js and consts.js to get from there and set there const, "hand written" values from whole project

---
+++ src/app/data/consts.js
@@ -0,0 +1,33 @@
+class Consts {
+  static GAME_MODES = {
+    students: {
+      gamemode: 'students',
+      question: 'Who is this student?',
+      urlAdress: 'http://hp-api.herokuapp.com/api/characters/students',
+      ids: '28',
+      answerProperty: 'name',
+    },
+    staff: {
+      gamemode: 'students',
+      question: 'Who is this staff?',
+      urlAdress: 'http://hp-api.herokuapp.com/api/characters/staff',
+      ids: '20',
+      answerProperty: 'name',
+    },
+    houses: {
+      gamemode: 'students',
+      question: 'From which house is this student?',
+      urlAdress: 'http://hp-api.herokuapp.com/api/characters/students',
+      ids: '28',
+      answerProperty: 'house',
+    },
+  };
+
+  static DEFAULT_BASE_URL = Consts.GAME_MODES.students.urlAdress;
+
+  static ONE_SECOND_MILLIS = 1000;
+
+  static DEFAULT_MAX_TIME = 2; // in minutes
+}
+
+export default Consts;
