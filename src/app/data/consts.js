5eff1c88958f471039e9578c8558cb7c9b4f152e
Monika Krella <monika.krella@gmail.com>
2021-12-12 23:36:32 +0100

    -amend to previous commit

--- src/app/data/consts.js
+++ src/app/data/consts.js
@@ -1,41 +1,40 @@
-export GAME_MODES = {
-    Students: {
-      gamemode: 'Students',
-      imgSubfolder: 'students',
-      question: 'Who is this student?',
-      urlAdress: 'http://hp-api.herokuapp.com/api/characters/students',
-      ids: '28',
-      answerProperty: 'name',
-    },
-    Staff: {
-      gamemode: 'Staff',
-      imgSubfolder: 'staff',
-      question: 'Who is this staff?',
-      urlAdress: 'http://hp-api.herokuapp.com/api/characters/staff',
-      ids: '20',
-      answerProperty: 'name',
-    },
-    Houses: {
-      gamemode: 'Houses',
-      imgSubfolder: 'students',
-      question: 'From which house is this student?',
-      urlAdress: 'http://hp-api.herokuapp.com/api/characters/students',
-      ids: '28',
-      answerProperty: 'house',
-    },
-  };
+export const GAME_MODES = {
+  Students: {
+    gamemode: 'Students',
+    imgSubfolder: 'students',
+    question: 'Who is this student?',
+    urlAdress: 'http://hp-api.herokuapp.com/api/characters/students',
+    ids: '28',
+    answerProperty: 'name',
+  },
+  Staff: {
+    gamemode: 'Staff',
+    imgSubfolder: 'staff',
+    question: 'Who is this staff?',
+    urlAdress: 'http://hp-api.herokuapp.com/api/characters/staff',
+    ids: '20',
+    answerProperty: 'name',
+  },
+  Houses: {
+    gamemode: 'Houses',
+    imgSubfolder: 'students',
+    question: 'From which house is this student?',
+    urlAdress: 'http://hp-api.herokuapp.com/api/characters/students',
+    ids: '28',
+    answerProperty: 'house',
+  },
+};
 
-export rankingsTemplate = {
-    students: [],
-    staff: [],
-    houses: [],
-  };
+export const RANKINGS_TEMPLATE = {
+  students: [],
+  staff: [],
+  houses: [],
+};
 
-export DEFAULT_BASE_URL = Consts.GAME_MODES.students.urlAdress;
+export const DEFAULT_BASE_URL = GAME_MODES.students.urlAdress;
 
-export ONE_SECOND_MILLIS = 1000;
+export const ONE_SECOND_MILLIS = 1000;
 
-export ONE_MINUTE_SECONDS = 60;
+export const ONE_MINUTE_SECONDS = 60;
 
-export DEFAULT_MAX_TIME = 2; // in minutes
-
+export const DEFAULT_MAX_TIME = 2; // in minutes
