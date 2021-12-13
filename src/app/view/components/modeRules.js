1190daa656be3e44c5c724c8bdbf48f35f04a2d6
Tomasz <skretkowskitomasz@gmail.com>
2021-12-13 18:02:25 +0100

    refactored modeRules file

--- src/app/view/components/modeRules.js
+++ src/app/view/components/modeRules.js
@@ -1,35 +1,34 @@
-import elementCreator from "../../utils/elementCreator";
-import elementInjector from "../../utils/elementInjector";
+import elementCreator from '../../utils/elementCreator';
 
-const rulesTextBoiler = "You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who is this Hogwart's student You see on the picture, on the left(someone right now) from availible options.";
+const rulesTextBoiler =
+  "You have one minute (1m) to answer as many questions as possible. During the game on each question you need to select who is this Hogwart's student You see on the picture, on the left(someone right now) from availible options.";
 
 const modeRules = () => {
-    const rulesIcon = elementCreator(
-        'span',
-        {class: 'rules-icon fas fa-graduation-cap'},
-    );
+  const rulesIcon = elementCreator('span', {
+    class: 'rules-icon fas fa-graduation-cap',
+  });
 
-    const rulesHeader = elementCreator(
-        'h2',
-        {class: 'game__mode-rules-header'},
-        rulesIcon,
-        'Mode Rules',
-    );
+  const rulesHeader = elementCreator(
+    'h2',
+    { class: 'game__mode-rules-header' },
+    rulesIcon,
+    'Mode Rules',
+  );
 
-    const rulesText = elementCreator(
-        'p',
-        {class: 'game__mode-rules-text'},
-        `${rulesTextBoiler}`
-    );
+  const rulesText = elementCreator(
+    'p',
+    { class: 'game__mode-rules-text' },
+    `${rulesTextBoiler}`,
+  );
 
-    const rulesFullElement = elementCreator(
-        'div',
-        {class: 'game__mode-rules'},
-        rulesHeader,
-        rulesText,
-    );
+  const rulesFullElement = elementCreator(
+    'div',
+    { class: 'game__mode-rules' },
+    rulesHeader,
+    rulesText,
+  );
 
-    return rulesFullElement;
-}
+  return rulesFullElement;
+};
 
 export default modeRules;
