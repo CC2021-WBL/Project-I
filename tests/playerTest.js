e605b6ade4b375d48d9702b8aeb145e7af7be223
Monika Krella <monika.krella@gmail.com>
2021-12-16 16:17:56 +0100

    -added player test to test player class

---
+++ tests/playerTest.js
@@ -0,0 +1,30 @@
+// connect this file to index.html, values will be on console
+
+/* eslint-disable import/extensions */
+import { Player } from '../src/app/logic/player.js';
+
+const player = new Player();
+
+player.registerAnswer('zombie', 'zombie', true);
+
+console.log(player.correctAnswersScore);
+console.log(JSON.parse(JSON.stringify(player.detailQuestionData)));
+// ^ zapis niezbędny, żeby wyświetlić zawartość w formie stringa(JSON), a nie przez referencje do tablicy
+console.log(player.detailQuestionData.length);
+
+// await sleep(8000);
+
+player.registerAnswer('zombie', 'yeti', false);
+
+console.log(player.detailQuestionData.length);
+const gameData = player.getPlayerQuestionsData();
+
+console.log(gameData);
+
+player.clearGameData();
+
+console.log(player.getPlayerQuestionsData());
+
+/* function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+} */
