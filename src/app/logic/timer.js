98f503da8b19e415631c712ab7d92e80520aed00
Monika Krella <monika.krella@gmail.com>
2021-12-17 18:45:55 +0100

    -removed comments from timer

--- src/app/logic/timer.js
+++ src/app/logic/timer.js
@@ -4,7 +4,7 @@ import { ONE_SECOND_MILLIS, ONE_MINUTE_SECONDS } from '../data/consts.js';
 class GameTimer {
   constructor(gameTime) {
     this.gameTime = gameTime;
-    this.stopGame = false; // zmiana wartości z zewnątrz zatrzyma timer?
+    this.stopGame = false;
   }
 
   setGameTime(newGameTime) {
@@ -25,13 +25,10 @@ class GameTimer {
       callbackOnInterval(timeLeftInSec, totalTimeInSec);
       if (timeLeft <= 0 || this.stopGame === true) {
         clearInterval(timer);
-        timeLeft = initialTimeInMilisec; // restet do wartości pierwotnej
+        timeLeft = initialTimeInMilisec;
         callbackOnEndOfTime();
-        // FINISH GAME FUNCTION / DISPLAY GAME OVER MODAL -- z innych miejsc podpięcie się pod ten callback
       }
     }, timeInterval);
   }
 }
 export default GameTimer;
-
-// DZIAŁA I CALLBACKI SIE WYWOŁUJĄ
