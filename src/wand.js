23f7e85010871abfb35778de81051adf2326d0ab
KatarzynaChmielecka <k.chmielecka.6@gmail.com>
2021-12-18 21:38:53 +0100

    Remove comments and white spaces

--- src/wand.js
+++ src/wand.js
@@ -1,4 +1,4 @@
-// const progressBar = document.querySelector('.game-timer__wand');
+/* eslint-disable no-plusplus */
 const inner = document.querySelector('.inner');
 const start = document.querySelector('.start');
 
@@ -29,24 +29,16 @@ const start = document.querySelector('.start');
 // };
 
 const timeStart = 60;
-// const checkColors = (width) => {
-//   if (width > 60) {
-//     inner.style.border = 'solid green';
-//   } else {
-//     inner.style.border = 'solid  red';
-//   }
-// };
-const bbb = () => {
+
+const wand = () => {
   let time = timeStart;
 
   const countDown = setInterval(() => {
-    // eslint-disable-next-line no-plusplus
     time--;
     const progressWidth = (time / timeStart) * 100;
 
     if (time > 0) {
       inner.style.width = `${progressWidth}%`;
-      // checkColors(progressWidth);
     } else {
       clearInterval(countDown);
       inner.style.width = '0%';
@@ -55,4 +47,4 @@ const bbb = () => {
   }, 1000);
 };
 
-start.addEventListener('click', bbb);
+start.addEventListener('click', wand);
