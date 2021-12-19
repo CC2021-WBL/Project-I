e9c982cc100b5a8ef9d927ce02a27ae5860d07ac
Monika Krella <monika.krella@gmail.com>
2021-12-19 21:57:26 +0100

    -changed name od gameMaker file -updated gameMakerTest

--- tests/gameMakerTest.js
+++ tests/gameMakerTest.js
@@ -2,11 +2,27 @@
 
 /* eslint-disable import/named */
 // eslint-disable-next-line import/extensions
-import GameMaker from '../src/app/logic/bigBossGameMaker.js';
+import GameMaker from '../src/app/logic/gameMaker.js';
 // eslint-disable-next-line import/extensions
 import { GAME_MODES } from '../src/app/data/consts.js';
 
 const gameModesProperties = Object.getOwnPropertyNames(GAME_MODES);
+
+// ----deklaracje funckji----------------------------------------------------------------------------
+const WrappedStartGameAndGetFirstQuestion = async () => {
+  const question = await gameMaker.startGameAndGetFirstQuestion(
+    doAtInterval,
+    doSomethingAtTheEndOfTime,
+  );
+  console.log(`first question: ${question}`);
+  return question;
+};
+
+const wrappedNextQuestion = async () => {
+  const nextQuestion = await gameMaker.createQuestion();
+  console.log(nextQuestion);
+  return nextQuestion;
+};
 
 function doSomethingAtTheEndOfTime() {
   console.log('-----------------the end-----------------------');
@@ -16,23 +32,17 @@ function doAtInterval(timeLeftInSec, totalTimeInSec) {
   console.log(`time left from timer: ${timeLeftInSec} from ${totalTimeInSec}`);
 }
 
+// ---wywoływanie------------------------------------------------------------------------------------
 const gameMaker = new GameMaker(gameModesProperties[0], 0.5);
 
-// works in live server
-const question = await gameMaker.startGameAndGetFirstQuestion(
-  doAtInterval,
-  doSomethingAtTheEndOfTime,
-);
-console.log(`first question: ${question}`);
+const question = WrappedStartGameAndGetFirstQuestion();
 
 // works in live server
 const isAnswerCorrect = gameMaker.checkAndRegisterAnswer('Heremione');
 console.log(`is answer correct? ${isAnswerCorrect}`);
 console.log(gameMaker.player.detailQuestionData);
 
-// works in live server
-const nextQuestion = await gameMaker.createQuestion();
-console.log(nextQuestion);
+const nextQuestion = wrappedNextQuestion().then;
 
 // works in live server
 const data = gameMaker.getEndGameData();
