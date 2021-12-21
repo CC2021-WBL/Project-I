30396f79cc780fee3c4c4f100d4d4c1c471cc07c
Smyku6 <smyku6@gmail.com>
2021-12-21 01:49:21 +0100

    first draft

---
+++ src/app/view/components/answersButtons.js
@@ -0,0 +1,32 @@
+import elementCreator from '../../utils/elementCreator';
+
+/**
+ * At this moment our parameter for this function is question
+ * @param question
+ * question as an object with answers property (answers as array)
+ * this function create buttons and return an array of buttons
+ * @returns {*[]}
+ */
+
+function answersButtons(question) {
+  const buttons = [];
+  question.answers.forEach((button, index) => {
+    buttons.push(
+      elementCreator(
+        'button',
+        { class: 'answerButton' },
+        question.answers[index],
+      ),
+    );
+  });
+
+  // buttons.map((button) =>
+  //   button.addEventListener('click', () => {
+  // @TODO here we add in the future some function
+  //   }),
+  // );
+
+  return buttons;
+}
+
+export default answersButtons;
