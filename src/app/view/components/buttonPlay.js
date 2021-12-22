f7fc09c5ba2e0778931f44179e52bdb52b9acba1
Alicja Kondrat <pierwszazlewej@gmail.com>
2021-12-22 20:02:45 +0100

    -adding new styles to buttons

--- src/app/view/components/buttonPlay.js
+++ src/app/view/components/buttonPlay.js
@@ -1,7 +1,11 @@
 import elementCreator from '../../utils/elementCreator';
 
 function buttonPlay(text) {
-  const buttonCreator = elementCreator('button', { class: 'buttonPlay' }, text);
+  const buttonCreator = elementCreator(
+    'button',
+    { class: 'game__button game__button--orange' },
+    text,
+  );
   return buttonCreator;
 }
 export default buttonPlay;
