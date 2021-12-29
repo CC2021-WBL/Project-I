ff45636cf94e5c2b1137e4162f444b80e2f59d49
Monika Krella <monika.krella@gmail.com>
2021-12-29 01:27:05 +0100

    -removed unnecessary lines from model.js (default time - it is inside default difficulty level, and import of GameMaker, which is declared in controller)

--- src/app/logic/model.js
+++ src/app/logic/model.js
@@ -1,12 +1,10 @@
-import { DEFAULT_DIFFICULTY_LEVEL, DEFAULT_TIME } from '../data/consts';
-import GameMaker from './gameMaker';
+import { DEFAULT_DIFFICULTY_LEVEL } from '../data/consts';
 import LocalStorageService from './localStorage';
 
 class Model {
   constructor() {
     this.gameMode = '';
     this.difficultyLevel = DEFAULT_DIFFICULTY_LEVEL;
-    this.gameTime = DEFAULT_TIME;
     this.rankings = LocalStorageService.loadRankings();
     this.localStorageService = LocalStorageService;
     this.gameMaker = undefined;
