3a891a15d93bf0a4fae63aa69bb6008f4114521a
Tomasz <skretkowskitomasz@gmail.com>
2021-12-29 17:12:26 +0100

    #86/  quit game and anserw buttons styles fixes

--- src/app/view/components/displayQuitGameButton.js
+++ src/app/view/components/displayQuitGameButton.js
@@ -1,6 +1,11 @@
 import elementCreator from '../../utils/elementCreator';
 
 export default function displayQuitGameButton() {
+  // @TODO napisz funkcję która podmienia style
+  const backChange = document.querySelector('.game__mode-rules');
+  backChange.style.background = 'none';
+  backChange.style.boxShadow = 'none';
+  // -----------
   const quitElem = elementCreator('i', {
     class: 'far fa-times-circle',
   });
