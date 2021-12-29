1b27c314e7274d4dccb583ea07a0697ea6522082
Monika Krella <monika.krella@gmail.com>
2021-12-29 19:14:13 +0100

    -changes in calculatePoints related to some  review suggestions

--- src/app/logic/calculatePoints.js
+++ src/app/logic/calculatePoints.js
@@ -6,20 +6,26 @@ export function calculatePoints(
   isAnswerCorrect,
   difficultyLevelsProperty,
 ) {
-  if (
-    DIFFICULTY_LEVELS[difficultyLevelsProperty].calculatingPoints ===
-    DIFFICULTY_LEVELS.medium.calculatingPoints
-  ) {
+  const difficultyLevelsProperties =
+    Object.getOwnPropertyNames(DIFFICULTY_LEVELS);
+
+  const isMediumMode =
+    difficultyLevelsProperty === difficultyLevelsProperties[1];
+  const isHardMode = difficultyLevelsProperty === difficultyLevelsProperties[2];
+  if (!isMediumMode && !isHardMode) {
+    throw new Error(
+      'wrong difficulty level property or calculatingPoints value',
+    );
+  }
+
+  if (isMediumMode) {
     let newCorrectAnswersScore = correctAnswersScore;
     if (isAnswerCorrect) {
       newCorrectAnswersScore = correctAnswersScore + 1;
     }
     return newCorrectAnswersScore;
   }
-  if (
-    DIFFICULTY_LEVELS[difficultyLevelsProperty].calculatingPoints ===
-    DIFFICULTY_LEVELS.hard.calculatingPoints
-  ) {
+  if (isHardMode) {
     let newCorrectAnswersScore = correctAnswersScore;
     if (isAnswerCorrect) {
       newCorrectAnswersScore += 1;
