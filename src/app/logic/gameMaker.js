22a94cac45f476c4f5cb1d9b75ba5e9ac5477ec6
Monika Krella <monika.krella@gmail.com>
2022-01-01 20:11:59 +0100

    -changes to send by detailEndGameData also base64 to image

--- src/app/logic/gameMaker.js
+++ src/app/logic/gameMaker.js
@@ -38,6 +38,7 @@ class GameMaker {
 
   checkAndRegisterAnswer(answer) {
     const isAnswerCorrect = this.player.checkAndRegisterAnswer(
+      this.questionObject.image,
       answer,
       this.questionObject.rightAnswer,
       this.difficultyLevelsProperty,
