a6a0d0be2a4c93e48ff216e87d0c070bb716196e
Tomasz <skretkowskitomasz@gmail.com>
2022-01-01 22:31:58 +0100

    #86/1 QuitGame UI and Logic

--- src/app/view/components/displayQuitGameButton.js
+++ src/app/view/components/displayQuitGameButton.js
@@ -2,9 +2,9 @@ import elementCreator from '../../utils/elementCreator';
 
 export default function displayQuitGameButton() {
   // @TODO napisz funkcję która podmienia style
-  const backChange = document.querySelector('.game__mode-rules');
-  backChange.style.background = 'none';
-  backChange.style.boxShadow = 'none';
+  // const backChange = document.querySelector('.game__mode-rules');
+  // backChange.style.background = 'none';
+  // backChange.style.boxShadow = 'none';
   // -----------
   const quitElem = elementCreator('i', {
     class: 'far fa-times-circle',
@@ -16,7 +16,6 @@ export default function displayQuitGameButton() {
     'Quit Game',
     quitElem,
   );
-  const clearNav = document.querySelector('.header__game-nav');
-  clearNav.style.visibility = 'hidden';
+
   return quitGameButton;
 }
