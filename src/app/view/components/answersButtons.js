f3d1518795d4c1d94ff8c3e4211473beb5741b01
Monika Krella <monika.krella@gmail.com>
2022-01-02 22:16:37 +0100

    documentation came back

--- src/app/view/components/answersButtons.js
+++ src/app/view/components/answersButtons.js
@@ -1,5 +1,13 @@
 import elementCreator from '../../utils/elementCreator';
 
+/**
+ * At this moment our parameter for this function is question
+ * @param question
+ * question as an object with answers property (answers as array)
+ * this function create buttons and return an array of buttons
+ * @returns {*[]}
+ */
+
 function answersButtons(question) {
   const buttons = [];
   question.answers.forEach((button, index) => {
