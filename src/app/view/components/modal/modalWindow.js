4f6ba6d24ea1b2a25d97983dbae24e3943570c66
Monika Krella <monika.krella@gmail.com>
2022-01-05 16:58:29 +0100

    -added function for changing text depend on result

--- src/app/view/components/modal/modalWindow.js
+++ src/app/view/components/modal/modalWindow.js
@@ -1,7 +1,6 @@
 import elementCreator from '../../../utils/elementCreator';
 import modalDetails from './modalDetails';
 import modalForm from './modalForm';
-import calculatePoints from '../../../logic/calculatePoints';
 
 function ModalWindow(gameData) {
   // parametry//
@@ -13,7 +12,7 @@ function ModalWindow(gameData) {
   const modalText = elementCreator(
     'p',
     { class: 'modal__text' },
-    `This is magic! You have answered ${gameData.askedQuestions} questions. Your score is ${gameData.correctAnswerScore}.`,
+    chooseText(gameData),
   );
 
   // DETAILS SECTION
@@ -37,6 +36,35 @@ function ModalWindow(gameData) {
   const modal = elementCreator('div', { class: 'modal' }, modalContainer);
 
   return modal;
+}
+
+function chooseText(gameData) {
+  const playerPoints = gameData.correctAnswerScore;
+  const questions = gameData.askedQuestions;
+  let text = '';
+  switch (true) {
+    case playerPoints < -10:
+      text = `Your result: ${playerPoints} / ${questions}. \n"Honestly, if you were any slower, you'd be going backward." — Draco Malfoy`;
+      break;
+
+    case playerPoints >= -10 && playerPoints < 1:
+      text = `Your result: ${playerPoints} / ${questions}. \n"I hope you're pleased with yourselves. We could all have been killed — or worse, expelled. Now if you don't mind, I'm going to bed." ― Hermione Granger`;
+      break;
+
+    case playerPoints >= 1 && playerPoints < 11:
+      text = `Your result: ${playerPoints} / ${questions}. \n"When in doubt, go to the library." — Ron Weasley " `;
+      break;
+
+    case playerPoints >= 11:
+      text = `Your result: ${playerPoints} / ${questions}. \n"You're a little scary sometimes, you know that? Brilliant ... but scary." — Ron Weasley`;
+      break;
+
+    default:
+      text = `This is magic! You've got ${playerPoints}/${questions}  questions! Congratulations!`;
+      break;
+  }
+
+  return text;
 }
 
 export default ModalWindow;
